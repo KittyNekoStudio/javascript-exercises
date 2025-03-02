900eb0a03231f5563fad03bce81cdba84b0c7a88
kittycat <kittynekostudio@gmail.com>
2025-03-01 16:44:05 -0800

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+	let ret = [];
+	for (const item of array) {
+		if (!args.includes(item)) {
+			ret.push(item);
+		}
+	}
+	return ret;
 };
 
 // Do not edit below this line
