5a22a392daeb10401ac020b940db5da979276b2a
kittycat <kittynekostudio@gmail.com>
2025-03-01 17:04:08 -0800

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+		return "ERROR";
+	}
 
+	let ret = 0;
+	for (let i = (start > end ? end : start); i <= (start > end ? start : end); i++) {
+		ret += i;
+	}
+
+	return ret;
 };
 
 // Do not edit below this line
